54ef3df297a8b360af806fec11bdde4a61579a10
jramos <juanramosgarcia@protonmail.com>
2019-11-08 09:59:44 +0100

    Initial commit with rest of files

---
+++ src/main/java/com/mempoolexplorer/txmempool/TxMemPoolApplication.java
@@ -0,0 +1,13 @@
+package com.mempoolexplorer.txmempool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TxMemPoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TxMemPoolApplication.class, args);
+	}
+
+}

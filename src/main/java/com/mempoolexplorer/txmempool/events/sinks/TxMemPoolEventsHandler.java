8c24b35de5c55dfb58a2a51b9cdfc09978204e6f
jramos <juanramosgarcia@protonmail.com>
2020-01-28 10:39:00 +0100

    Removed some imports causing alarms

--- src/main/java/com/mempoolexplorer/txmempool/events/sinks/TxMemPoolEventsHandler.java
+++ src/main/java/com/mempoolexplorer/txmempool/events/sinks/TxMemPoolEventsHandler.java
@@ -3,7 +3,6 @@ package com.mempoolexplorer.txmempool.events.sinks;
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
